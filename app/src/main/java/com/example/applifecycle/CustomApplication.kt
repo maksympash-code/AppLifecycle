4a5fabe997a7f36d463ec645450b89c67e5de850
maksympash <maksympash@gmail.com>
2025-10-07 17:15:54 +0300

    CustomApplication callbacks

--- app/src/main/java/com/example/applifecycle/CustomApplication.kt
+++ app/src/main/java/com/example/applifecycle/CustomApplication.kt
@@ -7,6 +7,8 @@ import android.util.Log
 
 class CustomApplication: Application() {
 
+    // adb shell ps -o PID,NAME | grep com.example.applifecycle
+
     class MyActivityLifecycleCallbacks: ActivityLifecycleCallbacks{
         override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
             Log.d(TAG2, "onActivityCreated: ${activity::class.simpleName}")
